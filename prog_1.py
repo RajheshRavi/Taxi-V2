b6c5b1b12170c9c103fdd21c9f1235e99c9b0804
RajheshRavi <41332510+RajheshRavi@users.noreply.github.com>
2018-12-20 13:05:44 +0530

    Update prog_1.py

--- prog_1.py
+++ prog_1.py
@@ -16,8 +16,8 @@ qTable=np.zeros([env.observation_space.n,env.action_space.n])
 
 for i in range(10000):
     
-    state = env.reset()
-    epochs, penality, reward = 0, 0, 0
+    state = env.reset()                                                         # Reseting the environment and obtaining States 
+    epochs, penality, reward = 0, 0, 0                                          # Initializing Penality and Reward
     done = False
     
     while not done:
@@ -25,28 +25,28 @@ for i in range(10000):
         env.render()
         
         if r.uniform(0,1) <= epsilon:
-            action = env.action_space.sample()
+            action = env.action_space.sample()                                  # Greedly traversing the states 
         else:
-            action = np.argmax(qTable[state])
+            action = np.argmax(qTable[state])                                   # Choosing the state which has highest reward
         
-        nextState,reward,done,info=env.step(action)
+        nextState,reward,done,info=env.step(action)                             # Performing the chosen action
     
         print("nextState",nextState,"reward",reward,"done",done,"info",info)
     
         oldVal=qTable[state][action]
         nextMax=np.max(qTable[nextState])
     
-        newVal=( 1 - alpha ) * oldVal + alpha * ( reward + gamma * nextMax )
-        qTable[state][action]=newVal
+        newVal=( 1 - alpha ) * oldVal + alpha * ( reward + gamma * nextMax )    
+        qTable[state][action]=newVal                                            # Updating the Qtable for the current state
     
-        state=nextState
+        state=nextState                                                         # Updating State
     
         print(reward)
     
         if reward == -10: 
-            penality+=1
+            penality+=1                                                         # Incrementing the penality
         
-        epochs+=1
+        epochs+=1                                                              
     
     
     
